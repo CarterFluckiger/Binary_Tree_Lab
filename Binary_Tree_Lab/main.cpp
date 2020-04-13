2d8797ddddfeec5f7363dd870612c62bdb6e0a7f
Carter Fluckiger <fluckiger2@tritions.iowacentral.edu>
2020-04-13 13:16:36 -0500

    Initial Commit

---
+++ Binary_Tree_Lab/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Binary_Tree_Lab
+//
+//  Created by Tag1 on 4/13/20.
+//  Copyright © 2020 Project. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
